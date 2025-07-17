2f6adca343b7112ff1142420f0d6d74366748d78
Pathomphong2607 <Pzt0935e@gmail.com>
2025-07-17 11:39:24 +0700

    week-3 commit

---
+++ week-3/ex1.py
@@ -0,0 +1,10 @@
+decimal = input("input demical here : ")
+decimal = int(decimal)
+
+binary = bin(decimal)
+hex = hex(decimal)
+oct = oct(decimal)
+
+print("this is binary ",binary)
+print("this is hex ",hex)
+print("this is oct ",oct)
